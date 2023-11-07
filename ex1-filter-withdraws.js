49d0eebf0faa3ef930b4efbcc0a11afc21fca02e
omiras <oscar.miras@gmail.com>
2023-11-07 10:20:06 +0100

    done

--- ex1-filter-withdraws.js
+++ ex1-filter-withdraws.js
@@ -7,6 +7,13 @@
  */
 function filterWithdraws(movements) {
   // TODO: Implementame
+  // 1. Esta función necesita un return
+  // 2. El método es el adecuado para esta función
+  // 3. Puede ser util que utilices una variable auxiliar para guardar el resultado del método filter
+  const allNegativeNumbers = movements.filter((n) => {
+    return n < 10;
+  }); // TODO: Completar derecha de la flecha
+  return allNegativeNumbers;
 }
 
 // Caso de prueba 1: Array con varios movimientos de entrada y retirada.
@@ -19,7 +26,7 @@ const test2 = [200, 50, 100, 300];
 console.log("Caso de prueba 2:", filterWithdraws(test2));
 // Se espera que el resultado sea [] (ninguna retirada).
 
-// Caso de prueba 3: Array vacío.
+// Caso de prueba 3: Un movimiento negativo
 const test3 = [66, -11];
 console.log("Caso de prueba 3:", filterWithdraws(test3));
 // Se espera que el resultado sea [-11] .
--- ex2-map-withdraws.js
+++ ex2-map-withdraws.js
@@ -6,19 +6,36 @@
  *
  * @param {array} movements Array de numbers de movimientos bancarios
  */
-function bankStatement(movements) {}
+function bankStatement(movements) {
+  // 1. Pensad en que estructura JavaScript nos permite aplicar un condicional
+  const statements = movements.map((n) =>
+    n < 0 ? `Withdraw: ${n}` : `Diposit: ${n}`
+  );
+  return statements;
+}
+
+function bankStatementWithIf(movements) {
+  // 1. Pensad en que estructura JavaScript nos permite aplicar un condicional
+  const statements = movements.map((n) => {
+    if (n < 0) {
+      return `Withdraw: ${n}`;
+    }
+    return `Diposit: ${n}`;
+  });
+  return statements;
+}
 
 // Caso de prueba 1: Array con varios movimientos de entrada y retirada.
 const test1 = [100, -50, 20, -30, 10];
-console.log("Caso de prueba 1:", bankStatement(test1));
-// Se espera que el resultado sea ["Deposit: 100", "Withdraw: 50", "Deposit: 20", "Withdraw: 30", "Deposit: 10"].
+console.log("Caso de prueba 1:", bankStatementWithIf(test1));
+// Se espera que el resultado sea ["Deposit: 100", "Withdraw: -50", "Deposit: 20", "Withdraw: -30", "Deposit: 10"].
 
 // Caso de prueba 2: Array con solo movimientos de entrada.
 const test2 = [200, 50, 100, 300];
-console.log("Caso de prueba 2:", bankStatement(test2));
+// console.log("Caso de prueba 2:", bankStatement(test2));
 // Se espera que el resultado sea ["Deposit: 200", "Deposit: 50", "Deposit: 100", "Deposit: 300"].
 
 // Caso de prueba 3: Array vacío.
 const test3 = [];
-console.log("Caso de prueba 3:", bankStatement(test3));
+// console.log("Caso de prueba 3:", bankStatement(test3));
 // Se espera que el resultado sea [] (ningún movimiento en el array).
